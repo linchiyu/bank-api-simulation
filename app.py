199c1ccb1204ef6c165aed64e7af468639504867
Lin Chi Yu <linchiyu1994@gmail.com>
2024-08-21 16:39:04 -0300

    fix: balance routes

--- app.py
+++ app.py
@@ -14,22 +14,48 @@ app = Flask(__name__)
 accounts: AccountManager = AccountManager()
 
 
-@app.route("/reset", methods=["POST"])
+@app.route("/reset", methods=["GET", "POST"])
 def reset():
     accounts.reset()
-    return "<p>Reset</p>"
+    return "OK", 200
 
 
-@app.route("/balance?account_id=<account_id>", methods=["GET"])
-def balance(account_id):
+@app.route("/balance", methods=["GET"])
+def balance():
+    account_id = request.args.get('account_id', default = '', type = str)
     account = accounts.get_account(account_id)
     if account is None:
-        return Response("0", status=HTTPStatus.NOT_FOUND)
-    return Response(str(balance), status=HTTPStatus.OK)
+        return Response("0", HTTPStatus.NOT_FOUND, mimetype='text/plain')
+    return Response(str(account.get_balance()), mimetype='text/plain', status=HTTPStatus.OK)
 
 
+@app.route("/event", methods=["POST"])
+def event_handler():
+    global accounts
+    event = request.get_json()
+    if event["type"] == "deposit":
+        account = accounts.deposit(event["destination"], event["amount"])
+        return Response(json.dumps({"destination": {"id": account.id, "balance": account.get_balance()}}),
+                        status=HTTPStatus.CREATED)
+    elif event["type"] == "withdraw":
+        account = accounts.withdraw(event["origin"], event["amount"])
+        if account is None:
+            return Response("0", status=HTTPStatus.NOT_FOUND)
+        return Response(json.dumps({"origin": {"id": account.id, "balance": account.get_balance()}}),
+                        status=HTTPStatus.CREATED)
+    elif event["type"] == "transfer":
+        origin, destination = accounts.transfer(event["origin"], event["destination"], event["amount"])
+        if origin is None:
+            return Response("0", status=HTTPStatus.NOT_FOUND)
+        return Response(json.dumps({"origin": {"id": origin.id, "balance": origin.get_balance()},
+                                    "destination": {"id":destination.id, "balance": destination.get_balance()}}),
+                        status=HTTPStatus.CREATED)
+    else:
+        return Response("0", status=HTTPStatus.BAD_REQUEST)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+app.run()
